0cf4cd8e3f7e922a16bd300eef5683862679c1f4
Liu Yu <liuyu@comp.nus.edu.sg>
2022-11-10 20:22:51 +0800

    refine code

--- src/add_facts.py
+++ src/add_facts.py
@@ -7,12 +7,6 @@ def get_symbolic_facts(declarations, k):
     Create `k` symbolic facts for each edb of passed `declarations`, where `declarations` is a sub dictionary of all declarations.
     """
 
-    facts = dict()
-    # TODO: types may be useful in future, so they are left here.
-    for (edb, types) in declarations.items():
-        values = list()
-        for _ in range(k):
-            values.append(common.ALPHA + str(utils.get_id()))
-        facts[edb] = values
+    facts = {predicate: [f"{common.ALPHA}{utils.get_id()}" for _ in range(k)] for (predicate, _) in declarations.items()}
 
     return facts
