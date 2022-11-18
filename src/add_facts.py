7e54f7c2e6c0fdf706829663f214c4b0b6b2c4b4
ly <liuyu@comp.nus.edu.sg>
2022-11-18 09:18:31 +0800

    domain facts

--- src/add_facts.py
+++ src/add_facts.py
@@ -1,93 +1,81 @@
 import src.common as common
-import src.utils as utils # Path: src/utils.py
+from souffle import collect, transform, Variable, Literal, Rule, Unification, parse, pprint, run_program
 import itertools
 
 
-def get_symbolic_facts(declarations, k):
-    """
-    Create `k` symbolic facts for each edb of passed `declarations`, where `declarations` is a sub dictionary of all declarations.
-    """
-
-    symbolic_facts = {predicate: [[f"{common.ALPHA}{utils.get_id()}" for _ in range(k)] for _ in range(len(args))] for (predicate, args) in declarations.items()}
-
-    return symbolic_facts
-
-
 def transform_into_meta_program(p):
-    symbolic_consts = collect(p, lambda x: x if isinstance(x, Variable) and x.name.startswith(common.SYMBOLIC_CONST_PREFIX))
-    binding_vars = [Variable(f"{common.BINDING_VARIABLE_PREFIX}{x.name}") for x in symbolic_consts]
+    """
+    Create new rules for EDB goals. `k` is the number of symbolic facts for each edb. The rules have two types: 1. e1(1,
+    2, t1, t2) :- domain_alpha(t1), domain_beta(t2). e1(1, 2) is the original fact. 2. e1(t1, t2, t1, t2) :-
+    domain_alpha(t1), domain_beta(t2). domain_x refers to the domain of symbol x. E.g.: domain_alpha(1).
+    domain_alpha('gamma!=alpha, gamma!=beta').
+    """
+
+    symbolic_consts = collect(p, lambda x: isinstance(
+        x, Variable) and x.name.startswith(common.SYMBOLIC_CONSTANT_PREFIX))
+    binding_vars = [
+        Variable(f"{common.BINDING_VARIABLE_PREFIX}{x.name}") for x in symbolic_consts]
+
+    facts = collect(p, lambda x: isinstance(x, Rule) and len(x.body) == 0)
+    relation_symbolic_consts_list_map = {rel_name: [f.head.args for f in facts if f.head.name == rel_name and f.head.args[0].name.startswith(
+        common.SYMBOLIC_CONSTANT_PREFIX)] for rel_name in [f.head.name for f in facts]}
+
+    # Create all symbolic constraints. E.g., symbolic consts are alpha, beta, gamma. Then, we create the following
+    # constraints: alpha != beta, alpha != gamma, beta != gamma, alpha = beta, alpha != gamma, beta != gamma, alpha !=
+    # beta, alpha = gamma, beta != gamma, alpha != beta, alpha != gamma, beta = gamma, alpha = beta, alpha = gamma, beta
+    # != gamma, alpha = beta, alpha != gamma, beta = gamma, alpha != beta, alpha = gamma, beta = gamma, all pairs of
+    # symbolic constants
+    sym_pairs = list(itertools.combinations(symbolic_consts, 2))
+    eq_pair_list_iter = [list(itertools.combinations(sym_pairs, k)) for k in range(0, len(
+        symbolic_consts) - 1)]  # the selected k pairs from sym_pairs are equal; the rest are not equal
+    symbolic_constrs_list = [[Unification(pair[0], pair[1], True if pair in eq_pair_list else False)
+                              for pair in sym_pairs] for eq_pair_list in eq_pair_list_iter]
 
     def add_binding_vars_to_literal(l):
-        return Literal(n.name, replaced_symbolic.args + binding_vars, n.positive)
+        return Literal(l.name, l.args + binding_vars, l.positive)
+
+    def add_symbol_domain_literal(v):
+        # E.g., domain_alpha(var_alpha)
+        return Literal(f"{common.DOMAIN_PREDICATE_PREFIX}{v.name}", [Variable(f"{common.BINDING_VARIABLE_PREFIX}{v.name}")], True)
+
+    def add_domain_facts(n):
+
+        def contains(sym_constr, v):
+            return v.name in sym_constr.left or v.name in sym_constr.right
+
+        if isinstance(n, Rule) and len(n.body) == 0:
+
+            if not n.head.args[0].name.startswith(common.SYMBOLIC_CONSTANT_PREFIX):
+                # n is a constant fact
+                const_facts = [Rule(Literal(f"{common.DOMAIN_PREDICATE_PREFIX}{v.name}", [f_const], True), [
+                ]) for sym_consts in relation_symbolic_consts_list_map[n.head.name] for (f_const, v) in zip(n.args, sym_consts)]
+                return const_facts
+            else:
+                # constraints for symbolic constants
+                symbolic_facts = [Rule(Literal(f"{common.DOMAIN_PREDICATE_PREFIX}{v.name}", list(filter(lambda sym_constr: contains(sym_constr, v), sym_constrs)), True), [
+                ]) for sym_consts in relation_symbolic_consts_list_map[n.head.name] for v in sym_consts for sym_constrs in symbolic_constrs_list]  # filter the symbolic constraints that contain the symbolic constant
+                return symbolic_facts
+        else:
+            return n
 
     def add_binding_vars(n):
         if isinstance(n, Rule):
-            if len(n.body) = 0:
+            if len(n.body) == 0:
                 # fact
-                replaced = transform(n.head, lambda x: f"{common.BINDING_VARIABLE_PREFIX}{x.name}" if x in symbolic_consts else n)
-                return Rule(add_binding_vars_to_literal(replaced), [])
+                replaced = transform(
+                    n.head, lambda x: f"{common.BINDING_VARIABLE_PREFIX}{x.name}" if x in symbolic_consts else x)
+                domain_literals = [add_symbol_domain_literal(x) for x in collect(n, lambda x: isinstance(
+                    x, Variable) and x.name.startswith(common.SYMBOLIC_CONSTANT_PREFIX))]
+                return Rule(add_binding_vars_to_literal(replaced), domain_literals)
             else:
                 # rule
-                transform(n.head, lambda x: add_binding_vars_to_literal(x) if isinstance(x, Literal) else x)
-    
-    return transform(p, add_binding_vars)
-    
+                transform(n.head, lambda x: add_binding_vars_to_literal(
+                    x) if isinstance(x, Literal) else x)
+                for l in n.body:
+                    transform(l, lambda x: add_binding_vars_to_literal(
+                        x) if isinstance(x, Literal) else x)
+                return n
+        else:
+            return n
 
-def get_EDB_rules(declarations, factsDirectory, k):
-    """
-    Create new rules for EDB goals. `k` is the number of symbolic facts for each edb.
-    The rules have two types:
-    1. e1(1, 2, t1, t2) :- domain_alpha(t1), domain_beta(t2). e1(1, 2) is the original fact. 
-    2. e1(t1, t2, t1, t2) :- domain_alpha(t1), domain_beta(t2).
-    domain_x refers to the domain of symbol x. E.g.:
-    domain_alpha(1).
-    domain_alpha('alpha').
-    """
-
-    nonsymbolic_facts = utils.load_relations(factsDirectory) # Load all facts from the facts directory
-    symbolic_facts = get_symbolic_facts(declarations, k) # Create `k` symbolic facts for each edb
-    temporary_vars = [f"{common.VAR}{i}" for i in range(1, k + 1)]
-    rules = []
-
-    for relation_name, tuples in nonsymbolic_facts.items():
-        for given_tuple in tuples:
-            rules.append(_get_EDB_predicate_rule(relation_name, given_tuple, temporary_vars, symbolic_facts[relation_name]))
-    rules.append(_get_EDB_predicate_rule(relation_name, temporary_vars, temporary_vars, symbolic_facts[relation_name]))
-
-    rules.extend(_get_domains(relation_name, nonsymbolic_facts, symbolic_facts))
-
-    return rules
-
-def _get_EDB_predicate_rule(relation_name, given_tuple, temporary_vars, symbolic_facts):
-    """
-    Create a rule for a given EDB relation name and tuples.
-    """
-
-    head_tuples = given_tuple + temporary_vars
-    head = "{relation_name}({tuples})".format(relation_name=relation_name, tuples=', '.join(head_tuples))
-    body = []
-
-    symbolic_vars = list(itertools.chain.from_iterable(symbolic_facts))
-
-    for i in range(len(temporary_vars)):
-        body.append("{domain}{symbolic_var}({temporary_var})".format(domain=common.DOMAIN, symbolic_var=symbolic_vars[i], temporary_var=temporary_vars[i]))
-
-    body = ":- {body}".format(body=', '.join(body)) if body else ""
-
-    return "{head}{body}.".format(head=head, body=body)
-
-def _get_domains(relation_name, non_symbolic_facts, symbolic_facts):
-    """
-    Create a rule for each domain of a given relation name.
-    """
-
-    non_symbolic_constants = list(itertools.chain.from_iterable(non_symbolic_facts[relation_name]))
-    symbolic_vars = list(itertools.chain.from_iterable(symbolic_facts[relation_name]))
-
-    domain_facts = []
-    for symbolic_var in symbolic_vars:
-        for non_symbolic_constant in non_symbolic_constants:
-            domain_facts.append(f"{common.DOMAIN}{symbolic_var}({non_symbolic_constant}).")
-            
-
-    return domain_facts
+    return transform(transform(p, add_binding_vars), add_domain_facts)
