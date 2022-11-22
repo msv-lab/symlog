4b180b36758cfb74e649e3fc5b9dc743e5275b54
Liu Yu <liuyu@comp.nus.edu.sg>
2022-11-22 12:26:58 +0800

    consts that sym consts try to unify with durg eval

--- src/common.py
+++ src/common.py
@@ -2,3 +2,5 @@ SYMBOLIC_CONSTANT_PREFIX = '_symlog_symbolic_'
 BINDING_VARIABLE_PREFIX = '_symlog_binding_'
 DOMAIN_PREDICATE_PREFIX = '_symlog_domain_'
 DEFAULT_SOUFFLE_TYPE = 'symbol'
+
+DELIMITER = ', '
--- src/transform_to_meta_program.py
+++ src/transform_to_meta_program.py
@@ -18,10 +18,128 @@ def constr_pred_consts_lst_map(facts, f):
     return d
 
 
+def constr_varef_joined_varefs_set_map(joined_varefs, f):
+    d = defaultdict(set)
+    for k, v in list(map(f, joined_varefs)):
+        if v:
+            d[k].add(v)
+    return d
+
+
+def analyse_symbolic_constants(p):
+    """Analyse the constansts that symbolic constants in program `p` in principle attmpte to unify with"""
+
+    facts = collect(p, lambda x: isinstance(x, Rule) and not x.body)
+    rules = collect(p, lambda x: isinstance(x, Rule) and x.body)
+    sym_facts = collect(p, lambda x: isinstance(x, Rule) and not x.body and any([isinstance(
+        arg, String) and str(arg.value).startswith(common.SYMBOLIC_CONSTANT_PREFIX) for arg in x.head.args]))
+
+    # varef -> (pred_name, index), values -> the set of possible values at index of pred
+    varef_values_map = defaultdict(set)
+    symconst_consts_map = dict()
+
+    def init_varef_values_map():
+        for fact in facts:
+            for i, arg in enumerate(fact.head.args):
+                varef_values_map[(fact.head.name, i)].add(arg)
+
+    def varef_values_of_head(rule):
+
+        def varef_values_of_arg(idx_arg):
+            idx, arg = idx_arg
+            varef = (rule.head.name, idx)
+
+            return varef, set(itertools.chain(*[varef_values_map[(body_lit.name, body_lit.args.index(arg))] for body_lit in rule.body if arg in body_lit.args and body_lit.positive]))
+
+        return list(map(varef_values_of_arg, enumerate(rule.head.args)))
+
+    def analyse_varef_values_of_program():
+
+        init_varef_values_map()
+        is_changed = True
+
+        while is_changed:
+            is_changed = False
+            for rule in rules:
+                for varef, values in varef_values_of_head(rule):
+                    is_changed = not varef_values_map[varef].issuperset(
+                        values) if not is_changed else True
+                    varef_values_map[varef].update(values)
+
+    def joined_varefs_of_sym_const_varef():
+
+        def extract_joined_varef_pair(l1, l2):
+            # l1 is the body literal has common variables with l2; l2 is the EDB literal uses symbolic constants
+            return [((l2.name, l2.args.index(arg)), (l1.name, l1.args.index(arg))) for arg in set(l2.args).intersection(set(l1.args))]
+
+        def joined_varefs_of_symbolic_fact(sym_fact):
+            rules_using_sym_fact = collect(p, lambda x: isinstance(x, Rule) and x.body and any(
+                [body_lit.name == sym_fact.head.name for body_lit in x.body if body_lit.positive]))  # positive body literals only
+
+            rule_lit_using_sym_fact = {
+                rule: body_lit for rule in rules_using_sym_fact for body_lit in rule.body if body_lit.name == sym_fact.head.name
+            }
+
+            varefs = []
+
+            for rule in rules_using_sym_fact:
+                lit_using_sym_fact = rule_lit_using_sym_fact[rule]
+                varefs.extend(
+                    list(itertools.chain(*map(lambda l: extract_joined_varef_pair(l, lit_using_sym_fact), filter(
+                        lambda l: set(l.args).intersection(set(lit_using_sym_fact.args)) and l != lit_using_sym_fact, rule.body))))
+                )
+
+            return varefs
+
+        joined_varefs = list(itertools.chain(
+            *map(joined_varefs_of_symbolic_fact, sym_facts)))
+
+        return constr_varef_joined_varefs_set_map(joined_varefs, lambda x: x)
+
+    def construct_try_unify_consts_map():
+
+        symvaref_joined_varefs_map = joined_varefs_of_sym_const_varef()
+
+        symvaref_joined_consts_map = {x: set(
+            itertools.chain(*[varef_values_map[varef] for varef in symvaref_joined_varefs_map[x]])) for x in symvaref_joined_varefs_map}
+
+        symbolic_consts = collect(p, lambda x: (isinstance(
+            x, String)) and x.value.startswith(common.SYMBOLIC_CONSTANT_PREFIX))
+
+        pred_sym_consts_list_map = constr_pred_consts_lst_map(
+            facts, lambda x: extract_pred_symbolic_consts(x, symbolic_consts))
+
+        pred_consts_list_map = constr_pred_consts_lst_map(
+            facts, lambda x: (x.head.name, x.head.args))
+
+        # sym varefs
+        sym_varefs = list()
+        for pred_name, sym_consts_list in pred_sym_consts_list_map.items():
+            if not sym_consts_list:
+                return
+            sym_varefs.extend(
+                [(pred_name, i) for i, _ in enumerate(sym_consts_list[0])]
+            )
+
+        for varef in sym_varefs:
+            # convert varef to symbolic constants
+            symconst_key = common.DELIMITER.join(
+                map(lambda x: x[varef[1]].value, pred_sym_consts_list_map[varef[0]]))
+
+            # union of in principle to be joined constants and the original constants
+            symconst_consts_map[symconst_key] = set(map(lambda x: x.value, symvaref_joined_consts_map[varef])) if varef in symvaref_joined_consts_map else set() | set(
+                map(lambda x: x[varef[1]].value, pred_consts_list_map[varef[0]]))
+
+    analyse_varef_values_of_program()  # analyse varef values of the program `p`
+    construct_try_unify_consts_map()
+
+    return symconst_consts_map
+
+
 def construct_naive_domain_facts(p):
 
     symbolic_consts = collect(p, lambda x: (isinstance(
-        x, String) or isinstance(x, Number)) and str(x.value).startswith(common.SYMBOLIC_CONSTANT_PREFIX))
+        x, String)) and x.value.startswith(common.SYMBOLIC_CONSTANT_PREFIX))
 
     facts = collect(p, lambda x: isinstance(x, Rule) and not x.body)
 
@@ -38,7 +156,7 @@ def construct_naive_domain_facts(p):
     for pred_name in pred_sym_consts_list_map.keys():
         for sym_consts, consts in itertools.product(pred_sym_consts_list_map[pred_name], pred_consts_list_map[pred_name]):
             const_facts.extend([construct_fact(f"{common.DOMAIN_PREDICATE_PREFIX}{sym_const.value}", [
-                               Variable(f"{const.value}")]) for (const, sym_const) in zip(consts, sym_consts)])
+                               const]) for (const, sym_const) in zip(consts, sym_consts)])
 
     return const_facts
 
@@ -48,7 +166,7 @@ def construct_abstract_domain_facts(p):
 
 
 def transform_into_meta_program(p):
-    """transform a Datalog program into the meta-Datalog program. E.g.,
+    """Transform a Datalog program into the meta-Datalog program. E.g.,
 
     Original Datalog program: 
 
@@ -234,3 +352,9 @@ variable("x").
     transformed.rules.extend(facts)
 
     print(pprint(transformed))
+
+    symconst_consts_map = analyse_symbolic_constants(program)
+
+    print("\n symconst_consts_map")
+    for k, v in symconst_consts_map.items():
+        print(k, v)
